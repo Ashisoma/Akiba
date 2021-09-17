7ccd37f7606b121442589d2953c9774fc93b886c
Ashisoma <okalionesmus@gmail.com>
2021-09-17 18:26:55 +0300

    first commit

---
+++ src/main/java/com/ashisoma/Akiba/AkibaApplication.java
@@ -0,0 +1,13 @@
+package com.ashisoma.Akiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkibaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashisoma/Akiba/AkibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashisoma.Akiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
