17d7d7b14fa6bf9390d37df32a67bb37f1410aa8
Ashisoma <okalionesmus@gmail.com>
2021-12-09 12:10:53 +0300

    loan controller and service

--- src/main/java/com/ashisoma/akiba/controller/LoanController.java
+++ src/main/java/com/ashisoma/akiba/controller/LoanController.java
@@ -1,7 +1,53 @@
 package com.ashisoma.akiba.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ashisoma.akiba.entity.Branch;
+import com.ashisoma.akiba.entity.Loan;
+import com.ashisoma.akiba.services.BranchService;
+import com.ashisoma.akiba.services.LoanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping
 public class LoanController {
+
+    @Autowired
+    private final LoanService service;
+
+    public LoanController(LoanService service) {
+        this.service = service;
+    }
+
+    // get methods
+    @GetMapping(path = "/get")
+    public List<Loan> getAll(){
+        return  service.getAllLoans();
+    }
+
+    @GetMapping(path = "/get/{id}")
+    public Optional<Loan> getById(@PathVariable(value = "id")Long id){
+        return  service.getById(id);
+    }
+    //post methods
+    @PostMapping(path = "/post")
+    public void addNew(@RequestBody Loan loan){
+        service.addNewLoan(loan);
+    }
+
+    // put methods
+    @PutMapping(path = "put/{id}")
+    public  void  updateBranch(@PathVariable(value = "id")Long id,
+                               @RequestParam(required = false) int loan_number,
+                               @RequestParam(required = false) Float loan_balance){
+        service.updateLoan(id,  loan_number, loan_balance);
+    }
+
+    //delete method
+    @DeleteMapping(path = "delete/{id}")
+    public  void deleteAnAccountById(Long id){
+        service.deleteLoanById(id);
+    }
 }
--- src/main/java/com/ashisoma/akiba/services/LoanService.java
+++ src/main/java/com/ashisoma/akiba/services/LoanService.java
@@ -1,7 +1,66 @@
 package com.ashisoma.akiba.services;
 
+import com.ashisoma.akiba.entity.Branch;
+import com.ashisoma.akiba.entity.Loan;
+import com.ashisoma.akiba.repository.BranchRepository;
+import com.ashisoma.akiba.repository.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class LoanService {
+    @Autowired
+    private final LoanRepository repository;
+
+
+    public LoanService(LoanRepository repository) {
+        this.repository = repository;
+    }
+
+    // GETTING ALL BRANCH
+    public List<Loan> getAllLoans(){
+        return repository.findAll();
+    }
+
+    // GETTING AN BRANCH BY ID
+    public Optional<Loan> getById(Long id){
+        return  repository.findById(id);
+    }
+
+    // ADD AN BRANCH
+    public  void addNewLoan(Loan loan){
+        Optional<Loan> loan1 = repository.findById(loan.getId());
+        if(loan1.isPresent()) {
+            throw new IllegalStateException("The branch exists");
+        } else {
+            repository.save(loan);
+        }
+    }
+
+    // UPDATE
+    @Transactional
+    public void updateLoan(Long id,  int loan_number, Float loan_balance) {
+        Loan loan = repository.findById(id).orElseThrow(() ->
+                new IllegalStateException("Loan with id: " + id + " does not exist"));
+        if (!Objects.equals(loan.getLoan_number(), loan_number)) {
+            loan.setLoan_number(loan_number);
+        }
+        if (loan_balance != null && loan_balance>0 && !Objects.deepEquals(loan.getLoan_number(), loan_balance)) {
+            loan.setLoan_balance(loan_balance);
+        }
+    }
+    //DELETE BY ID
+    public void  deleteLoanById(Long id){
+        boolean exists = repository.existsById(id);
+        if (!exists){
+            throw new IllegalStateException("Loan with id: " + id + "does not exist!");
+        }
+        else
+            repository.deleteById(id);
+    }
 }
